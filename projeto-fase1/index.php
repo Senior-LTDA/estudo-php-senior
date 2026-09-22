00fa9433133015a7e47cc12a0d531214638a45e0
danielsobral <danielsobralnascimento@hotmail.com>
2026-09-22 23:08:14 +0000

    fix(primeiro-projeto): modificando readme.md e index.php

--- projeto-fase1/index.php
+++ projeto-fase1/index.php
@@ -1,3 +1,16 @@
 <?php
 
-phpinfo();
+$name = "Daniel";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina de apresentação</title>
+</head>
+<body>
+    <?= "Olá, meu nome é: {$name}"?>
+</body>
+</html>
